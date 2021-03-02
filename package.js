25c1b4a6459812783d7ff768d656965a46978c26
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2021-03-02 00:28:53 -0500

    View a department displays department table successfully

--- package.js
+++ package.js
@@ -34,10 +34,11 @@ function start() {
     ])
     .then(function (answer) {
       if (answer.AddViewOrUpdate === "View a department") {
-        viewDepartment();
-      } else if (answer.AddViewOrUpdate === "View a role") {
-        viewRole();
+        displayCurrentDept();
       } 
+      // else if (answer.AddViewOrUpdate === "View a role") {
+      //   viewRole();
+      // } 
       // else if (answer.AddViewOrUpdate === "View an employee") {
       //   viewEmployee();
       // } else if (answer.AddViewOrUpdate === "Add a department") {
@@ -63,63 +64,3 @@ function displayCurrentDept() {
     console.log('-----------------------------------------------------------')
   });
 };
-
-function addDepartment() {
-  inquirer
-  .prompt([
-    {
-      name: 'deptName',
-      type: 'input',
-      message: 'Enter the department name'
-    }
-  ])
-  .then(function(answer) {
-    connection.query(
-      'INSERT INTO department SET ?',
-      {
-        dept_name: answer.deptName
-      },
-      function(err, res) {
-        if (err) throw err;
-        console.table(res)
-      }
-    );
-  });
-};
-
-// function addRole() {
-//   inquirer
-//   .prompt([
-//     {
-//       name: 'title',
-//       type:'input',
-//       message: 'Enter the role title'
-//     },
-//     {
-//       name: 'salary',
-//       type: 'input',
-//       message: 'Enter the employee salary'
-//     },
-//     {
-//       name: 'deptId',
-//       type: 'input',
-//       message: 'Enter the department ID'
-//     }
-//   ])
-//   .then(function(answer) {
-//     connection.query(
-//       'INSERT INTO role SET ?',
-//       {
-//         role_title: answer.title,
-//         role_salary: answer.salary,
-//         dept_id: answer.deptId
-//       },
-//       function(err) {
-//         if (err) throw err;
-//         console.log("Your role was added successfully.");
-//         console.table(role);
-//         start();
-//       }
-//     );
-//   });
-// };
