8355834af082191e7574f234b29a681f7b57b32a
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2021-03-01 21:55:07 -0500

    Resolved the connecting error with tutor Paolo

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  port: process.env.PORT || 3306,
+  // port: process.env.PORT || 3306,
   user: "root",
   password: "",
   database: "employee_mgmt",
