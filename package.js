be2e3484c314941dfe4328ba8a8c5b66ceed87bb
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2021-03-02 00:26:09 -0500

    Deleted nodemon and added the inquirer and mysql versions from past completed activities to see if this would fix the inquirer list on arrow key press. Success.

--- package.js
+++ package.js
@@ -3,16 +3,14 @@ const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  // port: process.env.PORT || 3306,
+  port: process.env.PORT || 3306,
   user: "root",
-  password: "",
+  password: "Nalabear1!",
   database: "employee_mgmt",
 });
 
 connection.connect(function (err) {
   if (err) throw err;
-  console.log("Welcome!");
-
   start();
 });
 
@@ -24,34 +22,104 @@ function start() {
         type: "list",
         message: "Please select what you would like to do",
         choices: [
-          "Add a department",
-          "Add a role",
-          "Add an employee",
           "View a department",
           "View a role",
           "View an employee",
-          "Update employee roles",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update employee roles"
         ],
       },
     ])
     .then(function (answer) {
-      if (answer.AddViewOrUpdate === "Add a department") {
-        addDepartment();
-      } else if (answer.AddViewOrUpdate === "Add a role") {
-        addRole();
-      } else if (answer.AddViewOrUpdate === "Add an employee") {
-        addEmployee();
-      } else if (answer.AddViewOrUpdate === "View a department") {
+      if (answer.AddViewOrUpdate === "View a department") {
         viewDepartment();
       } else if (answer.AddViewOrUpdate === "View a role") {
         viewRole();
-      } else if (answer.AddViewOrUpdate === "View an employee") {
-        viewEmployee();
-      } else if (answer.AddViewOrUpdate === "Update employee roles") {
-        updateEmployeeRole();
-      } else {
+      } 
+      // else if (answer.AddViewOrUpdate === "View an employee") {
+      //   viewEmployee();
+      // } else if (answer.AddViewOrUpdate === "Add a department") {
+      //   addDepartment();
+      // } else if (answer.AddViewOrUpdate === "Add a role") {
+      //   addRole();
+      // } else if (answer.AddViewOrUpdate === "Add an employee") {
+      //   addEmployee();
+      // } else if (answer.AddViewOrUpdate === "Update employee roles") {
+      //   updateEmployeeRole();
+      // } 
+      else {
         // otherwise end prompt
         connection.end();
-      }
+      };
     });
-}
+};
+
+function displayCurrentDept() {
+   connection.query('SELECT * FROM department', function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    console.log('-----------------------------------------------------------')
+  });
+};
+
+function addDepartment() {
+  inquirer
+  .prompt([
+    {
+      name: 'deptName',
+      type: 'input',
+      message: 'Enter the department name'
+    }
+  ])
+  .then(function(answer) {
+    connection.query(
+      'INSERT INTO department SET ?',
+      {
+        dept_name: answer.deptName
+      },
+      function(err, res) {
+        if (err) throw err;
+        console.table(res)
+      }
+    );
+  });
+};
+
+// function addRole() {
+//   inquirer
+//   .prompt([
+//     {
+//       name: 'title',
+//       type:'input',
+//       message: 'Enter the role title'
+//     },
+//     {
+//       name: 'salary',
+//       type: 'input',
+//       message: 'Enter the employee salary'
+//     },
+//     {
+//       name: 'deptId',
+//       type: 'input',
+//       message: 'Enter the department ID'
+//     }
+//   ])
+//   .then(function(answer) {
+//     connection.query(
+//       'INSERT INTO role SET ?',
+//       {
+//         role_title: answer.title,
+//         role_salary: answer.salary,
+//         dept_id: answer.deptId
+//       },
+//       function(err) {
+//         if (err) throw err;
+//         console.log("Your role was added successfully.");
+//         console.table(role);
+//         start();
+//       }
+//     );
+//   });
+// };
