428ebe64b4c6c4ac3a77463a0eb9a8188584cb7f
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2021-03-01 19:49:16 -0500

    Successfully created tables in my sql. Added in seed.sql

--- seed.sql
+++ seed.sql
@@ -4,7 +4,24 @@ CREATE DATABASE employee_mgmt;
 USE employee_mgmt;
 
 CREATE TABLE department(
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL,
     dept_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
-)
+);
+
+CREATE TABLE role(
+id INT NOT NULL,
+role_title VARCHAR(30) NOT NULL,
+role_salary DECIMAL(8,2) NOT NULL,
+dept_id INT NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+id INT NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT NULL,
+PRIMARY KEY(id)
+);
